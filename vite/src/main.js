905dbca3f42c062dbf9b838687800ad879b8dbec
Niki <niki.zhang770@gmail.com>
2026-01-03 22:00:06 -0500

    added geometry animation and orbit controls

--- vite/src/main.js
+++ vite/src/main.js
@@ -1,24 +1,58 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.getElementById('canvas');
 
-setupCounter(document.querySelector('#counter'))
+// 1. Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
+
+// 2. Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// 3. Object
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshBasicMaterial({ color: '#a13f5f', emissive: '#a13f5f' });
+const dodecahedron = new THREE.Mesh(geometry, material);
+
+const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
+const boxMaterial = new THREE.MeshBasicMaterial({ color: '#591f32', emissive: '#591f32' });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.y = -1.5;
+
+scene.add(dodecahedron);
+scene.add(box);
+
+// 4. Light
+const light = new THREE.SpotLight(0x006769, 10);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+// 5. Renderer
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio); // controls also work on mobile devices
+
+// 6. Add OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; // for smooth controls
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+// 7. Add Animations
+function animate() {
+    requestAnimationFrame(animate);
+
+    dodecahedron.rotation.x += 0.01;
+    dodecahedron.rotation.y += 0.01;
+
+    box.rotation.y += 0.005;
+
+    controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+
+    renderer.render(scene, camera);
+}
+
+animate();
